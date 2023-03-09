7e4e491cfeee8203baa2b696685311c4b63aee06
Marcos Santos <marcos.infosantos@gmail.com>
2023-03-09 13:22:33 -0300

    Initial commit

---
+++ App/Controller/Pages/Home.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Pages;
+
+class Home{
+
+    /**
+     * Metodo responsavel por retornar o View da nossa home
+     * @return string
+     */
+    public static function getHome(){
+        return 'Ola mundo';
+
+    }
+
+}
+
+?>
